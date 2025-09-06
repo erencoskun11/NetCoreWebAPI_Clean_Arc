23393e8008f4db1d1709bf139b8539711edfa13f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-06 17:33:42 +0300

    Created

---
+++ Repositories/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace App.Repositories
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Buraya migration çalıştırırken kullanılacak connection string'i yaz.
+            // (Güvenlik için production'ta burada şifre saklama. Sadece migration için uygun.)
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=NetCoreWebAPI_DB;Username=guest;Password=guest");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
