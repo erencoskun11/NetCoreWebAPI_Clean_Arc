5f7fdb107cb00110cbe397096b4b52a824e0c01b
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-08 15:08:54 +0300

    Created ProductsControllers

---
+++ App.API/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using App.Services.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResult<List<ProductDto>>(await _productService.GetAllListAsync());
+        }
+
+        [HttpGet("{pageNumber}/{pageSize}")]
+        public async Task<IActionResult> GetPagedAll(int pageNumber,int pageSize)
+        {
+            return CreateActionResult<List<ProductDto>>(await _productService.GetPagedAllListAsync(pageNumber,pageSize));
+        }
+
+
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResult<ProductDto?>(await _productService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProductRequest request)
+        {
+            return CreateActionResult<CreateProductResponse>(await _productService.CreateProductAsync(request));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateProductRequest request)
+        {
+            return CreateActionResult<object>(await _productService.UpdateProductAsync(id, request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResult<object>(await _productService.DeleteProductAsync(id));
+        }
+
+        [HttpGet("top/{count}")]
+        public async Task<IActionResult> GetTopPriceProducts(int count)
+        {
+            return CreateActionResult<List<ProductDto>>(await _productService.GetTopPriceAsync(count));
+        }
+    }
+}
+
