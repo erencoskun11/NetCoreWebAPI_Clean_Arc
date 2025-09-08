edfee12c66f173b4f6139b15fdc083a2f136dac0
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-08 15:17:00 +0300

    updated

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using App.Repositories.Products;
+
+namespace App.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll() => _dbSet.AsNoTracking();
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+
+        public ValueTask<T?> GetByIdAsync(int id) => _dbSet.FindAsync(id);
+
+        public async ValueTask<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+}
