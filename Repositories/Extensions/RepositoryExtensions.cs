911e2d64891f4e3e3d4ac18f76f1c7565e4eb374
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-08 15:13:16 +0300

    updated

--- Repositories/Extensions/RepositoryExtensions.cs
+++ Repositories/Extensions/RepositoryExtensions.cs
@@ -1,28 +1,18 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace App.Repositories.Extensions
 {
     public static class RepositoryExtensions
     {
-        public static IServiceCollection AddRepositories(this IServiceCollection services,IConfiguration configuration)
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<AppDbContext>(options =>
-            {
-                var connectionStrings = configuration.GetSection
-                (ConnectionStringOptions.Key).Get<ConnectionStringOptions>();
-                options.UseNpgsql(connectionStrings!.PostgreSql,postsqlOptionsAction =>
-                {
-                    postsqlOptionsAction.MigrationsAssembly(typeof(RepositoryAssambly).Assembly.FullName);
-                });
-            });
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            
             return services;
         }
     }
