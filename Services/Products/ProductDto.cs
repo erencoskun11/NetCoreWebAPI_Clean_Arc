38be6c2bcacf4da5d742d72d8a0c8f8d9e7262b6
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-09 15:33:43 +0300

    Updated

---
+++ Services/Products/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.Products
+{
+    public record ProductDto
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public int  Stock { get; init; }
+    }
+}
