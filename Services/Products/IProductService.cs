ea2bf114f13ba87d85876af1252bd75e96b83f44
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-09 15:33:01 +0300

    Updated

---
+++ Services/Products/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App.Services.Products
+{
+    public interface IProductService
+    {
+        Task<ServiceResult<List<ProductDto>>> GetTopPriceAsync(int count);
+        Task<ServiceResult<List<ProductDto>>> GetAllListAsync();
+        Task<ServiceResult<ProductDto?>> GetByIdAsync(int id);
+        Task<ServiceResult<CreateProductResponse>> CreateProductAsync(CreateProductRequest request);
+        Task<ServiceResult> UpdateProductAsync(int id, UpdateProductRequest request);
+        Task<ServiceResult> DeleteProductAsync(int id);
+        Task<ServiceResult<List<ProductDto>>> GetPagedAllListAsync(int pageNumber, int pageSize);
+
+    }
+}
