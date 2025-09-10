73d901a452a40c910219a88b6b6990effd769b29
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-10 14:52:50 +0300

    Created

---
+++ Services/FluentValidationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace App.Services
+{
+    public class FluentValidationFilter : IAsyncActionFilter
+    {        
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values
+                  .SelectMany(e => e.Errors)
+                  .Select(x => x.ErrorMessage).ToList();
+
+                context.Result = new BadRequestObjectResult(new { Errors = errors });
+
+                var resultModel = ServiceResult.Fail(errors);
+                //context.Result = new BadRequestObjectResult(resultModel);
+                return;
+            }
+            await next();
+        }
+    }
+}
