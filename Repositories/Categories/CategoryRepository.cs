7c907aef8c12fafa1b8c37c8faf6763e5c69074e
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-12 13:05:13 +0300

    Created

---
+++ Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace App.Repositories.Categories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Category> GetCategoryByProductsAsync()
+        {
+            return _context.Categories.Include(x=>x.Products).AsQueryable();
+        }
+
+        public Task<Category?> GetCategoryWithProductsAsync(int id)
+        {
+            return _context.Categories.Include(x => x.Products).FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
