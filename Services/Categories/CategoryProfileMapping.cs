8e46e40719f33880bb6b8891833f0516a1eb3a8b
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-13 13:49:19 +0300

    Updated

---
+++ Services/Categories/CategoryProfileMapping.cs
@@ -0,0 +1,30 @@
+﻿using App.Repositories.Categories;
+using App.Services.Categories.Create;
+using App.Services.Categories.Update;
+using AutoMapper;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace App.Services.Categories
+{
+    public class CategoryProfileMapping : Profile
+    {
+        public CategoryProfileMapping()
+
+        {
+            CreateMap<CategoryDto, Category>().ReverseMap();
+
+            CreateMap<Category, CategoryWithProductsDto>().ReverseMap();
+
+            CreateMap<CreateCategoryRequest,Category>().ForMember(dest =>dest.Name,
+                opt =>opt.MapFrom(src=>src.Name.ToLowerInvariant()));
+
+            CreateMap<UpdateCategoryRequest,Category>().ForMember(dest => dest.Name,
+                opt => opt.MapFrom(src => src.Name.ToLowerInvariant()));
+               
+                       
+
+
+
+        }
+    }
+}
