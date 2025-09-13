c9b96414e21c5389d7c7e88066572f8e83cc9796
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-13 13:53:53 +0300

    Created

---
+++ Services/ExceptionHandlers/CriticalExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace App.Services.ExceptionHandlers
+{
+    public class CriticalExceptionHandler() : IExceptionHandler
+    {
+        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            if(exception is CriticalException)
+            {
+                Console.WriteLine("An SMS regarding the error has been sent");
+            }
+         return ValueTask.FromResult(false);
+        }
+    }
+}
