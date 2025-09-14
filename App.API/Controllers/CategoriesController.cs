7847d4fc9ae313fc5636249dcfc13c51a5bfc4f7
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-14 13:04:02 +0300

    Created

---
+++ App.API/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using App.Services.Categories;
+using App.Services.Categories.Create;
+using App.Services.Categories.Dto;
+using App.Services.Categories.Update;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace App.API.Controllers
+{
+
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            return CreateActionResult(await _categoryService.GetAllListAsync());
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(int id) => CreateActionResult(await _categoryService.GetByIdAsync(id));
+
+        [HttpGet("products")]
+        public async Task<IActionResult> GetAllCategoriesWithProducts()
+    => CreateActionResult(await _categoryService.GetAllCategoriesWithProductsAsync());
+
+        [HttpGet("{id}/products")]
+        public async Task<IActionResult> GetCategoryWithProducts(int id)
+            => CreateActionResult(await _categoryService.GetCategoryWithProductsAsync(id));
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequest request)
+        {
+            return CreateActionResult(await _categoryService.CreateAsync(request));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, UpdateCategoryRequest request) => CreateActionResult(await
+            _categoryService.UpdateAsync(id, request));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id) =>CreateActionResult(await _categoryService.DeleteAsync(id));
+
+
+
+
+    } }
