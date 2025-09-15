10f356c74eb09f9a953eed547b0554cc81baf932
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-15 12:32:48 +0300

    Updated

--- Services/Categories/CategoryProfileMapping.cs
+++ Services/Categories/CategoryProfileMapping.cs
@@ -1,30 +1,26 @@
 ﻿using App.Repositories.Categories;
 using App.Services.Categories.Create;
+using App.Services.Categories.Dto;
 using App.Services.Categories.Update;
 using AutoMapper;
-using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace App.Services.Categories
 {
     public class CategoryProfileMapping : Profile
     {
         public CategoryProfileMapping()
-
         {
-            CreateMap<CategoryDto, Category>().ReverseMap();
-
+            CreateMap<Category, CategoryDto>().ReverseMap();
             CreateMap<Category, CategoryWithProductsDto>().ReverseMap();
 
-            CreateMap<CreateCategoryRequest,Category>().ForMember(dest =>dest.Name,
-                opt =>opt.MapFrom(src=>src.Name.ToLowerInvariant()));
+            CreateMap<CreateCategoryRequest, Category>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToLowerInvariant()))
+                .ForMember(dest => dest.Products, opt => opt.Ignore()); // Products alanını ignore et
 
-            CreateMap<UpdateCategoryRequest,Category>().ForMember(dest => dest.Name,
-                opt => opt.MapFrom(src => src.Name.ToLowerInvariant()));
-               
-                       
-
-
-
+            CreateMap<UpdateCategoryRequest, Category>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToLowerInvariant()))
+                .ForMember(dest => dest.Id, opt => opt.Ignore())        // Id'yi ignore et
+                .ForMember(dest => dest.Products, opt => opt.Ignore()); // Products alanını ignore et
         }
     }
 }
