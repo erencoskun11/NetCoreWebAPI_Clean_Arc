41d23899d429c6cf3328eede5fb4f18853e74835
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-20 19:54:00 +0300

    feat: add BusExtensions for MassTransit RabbitMQ configuration

---
+++ App.Bus/BusExtensions.cs
@@ -0,0 +1,35 @@
+﻿using App.Domain.Options;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace App.Bus
+{
+    public static class BusExtensions
+    {
+        public static void AddBus(this IServiceCollection services, IConfiguration configuration)
+        {
+            var serviceBusOptions = configuration
+                .GetSection(nameof(ServiceBusOptions))
+                .Get<ServiceBusOptions>();
+
+            services.AddMassTransit(x =>
+            {
+                // Eğer consumer’ların varsa burada ekleyebilirsin
+                // x.AddConsumer<ProductCreatedConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(new Uri(serviceBusOptions!.Url), h =>
+                    {
+                       
+                    });
+
+                    // Consumer’ları otomatik ekle
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+        }
+    }
+}
