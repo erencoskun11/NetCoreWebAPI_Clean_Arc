d9961e385ecaf88667d120790bfd09b0de25d479
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-21 18:06:40 +0300

    feat(api): add generic NotFoundFilter<T,TId> for action methods

---
+++ App.API2/Filters/NotFoundFilter.cs
@@ -0,0 +1,52 @@
+﻿using App.Application;
+using App.Application.Contracts.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace App.API2.Filters
+{
+    public class NotFoundFilter<T,TId> : Attribute,IAsyncActionFilter where T : class where TId : struct
+    {
+        private readonly IGenericRepository<T, TId> _repositoryRepository;
+        public NotFoundFilter(IGenericRepository<T, TId> genericRepository)
+        {
+            _repositoryRepository = genericRepository;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            var idValue = context.ActionArguments.Values.FirstOrDefault();
+
+            var idKey = context.ActionArguments.Keys.First();
+
+
+            if (idValue==null && idKey !="id")
+            {
+                await next(); return;
+            }
+
+            if (idValue is not TId id )
+            {
+                await next(); return;
+            }
+            var anyEntity = await _repositoryRepository.AnyAsync(id);
+
+
+            if (!anyEntity)
+            {
+                var entityName = typeof(T).Name;
+
+                //action method name
+                var actionName = context.ActionDescriptor.RouteValues["action"];
+
+                var result = ServiceResult.Fail($"Data not found({entityName})({actionName}).");
+                context.Result = new NotFoundObjectResult(new { Message = "Entity nor found" });
+                return;
+            }
+
+
+
+            await next();
+        }
+    }
+}
