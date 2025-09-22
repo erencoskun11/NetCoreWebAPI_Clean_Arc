113ad9aa7268840250abb0945504629bea16898f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-22 14:46:16 +0300

    feat(cache) : added CacheService implementation for in-memory caching

---
+++ App.Caching/CacheService.cs
@@ -0,0 +1,41 @@
+﻿using App.Application.Contracts.Caching;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Caching
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public CacheService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public Task AddAsync<T>(string cacheKey, T value, TimeSpan exprTimeSpan)
+        {
+            var cacheOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = exprTimeSpan
+            };
+            _memoryCache.Set(cacheKey, value, cacheOptions);
+            return Task.CompletedTask;
+        }
+
+        public Task<T> GetAsync<T>(string cacheKey)
+        {
+            if (_memoryCache.TryGetValue(cacheKey, out T cacheItem)) return Task.FromResult(cacheItem);
+            return Task.FromResult(default(T));
+        }
+
+        public Task RemoveAsync(string cacheKey)
+        {
+            _memoryCache.Remove(cacheKey);
+            return Task.CompletedTask;
+        }
+    }
+}
