4b1459d91e44a4c0a73cb55b3a815dab2f449ade
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-22 14:47:34 +0300

    feat(rabbitmq) : created

---
+++ App.Domain/Const/ServiceBusConst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Const
+{
+    public class ServiceBusConst
+    {
+        //<>app.<ecentname>.<queue-name>
+        public const string ProductAddedEventQueueName = "clean.app.productadded.event.queue";
+        public const string CategoryAddedEventQueueName = "clean.app.categoryadded.event.queue";
+    }
+}
