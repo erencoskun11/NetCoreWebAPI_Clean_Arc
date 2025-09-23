0d7cbce41004e4f38bc083aa1a7c740cb1911806
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-23 20:32:46 +0300

    feat(Mapping):Created

---
+++ App.Application/Features/Categories/CategoryProfileMapping.cs
@@ -0,0 +1,31 @@
+﻿// CategoryProfileMapping.cs
+using App.Application.Features.Categories.Create;
+using App.Application.Features.Categories.Dto;
+using App.Application.Features.Categories.Update;
+using App.Domain.Entities;
+using AutoMapper;
+
+namespace App.Application.Features.Categories
+{
+    public class CategoryProfileMapping : Profile
+    {
+        public CategoryProfileMapping()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryWithProductsDto>().ReverseMap();
+
+            CreateMap<CreateCategoryRequest, Category>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToLowerInvariant()))
+                .ForMember(dest => dest.Products, opt => opt.Ignore())
+                .ForMember(dest => dest.Created, opt => opt.Ignore())
+                .ForMember(dest => dest.Updated, opt => opt.Ignore());
+
+            CreateMap<UpdateCategoryRequest, Category>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToLowerInvariant()))
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Products, opt => opt.Ignore())
+                .ForMember(dest => dest.Created, opt => opt.Ignore())
+                .ForMember(dest => dest.Updated, opt => opt.Ignore());
+        }
+    }
+}
