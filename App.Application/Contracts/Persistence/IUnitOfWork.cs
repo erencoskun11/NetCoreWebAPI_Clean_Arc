bbd7e6d5debd64eb8361667e3d069358ba50e907
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-24 17:17:33 +0300

    feat(IUnitOfWork): add IUnitOfWork

---
+++ App.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Application.Contracts.Persistence
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    }
+}
