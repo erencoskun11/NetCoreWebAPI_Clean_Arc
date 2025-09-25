ac188d342534e63cc18717d8cd4f679ddbf88cc0
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-25 20:25:22 +0300

    feat(Mapping) : created

---
+++ App.Application/Features/Products/ProductsMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using App.Application.Features.Products.Create;
+using App.Application.Features.Products.Dto;
+using App.Application.Features.Products.Update;
+using App.Domain.Entities;
+using AutoMapper;
+
+namespace App.Application.Features.Products
+{
+    public class ProductsMappingProfile : Profile
+    {
+        public ProductsMappingProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+
+            CreateMap<CreateProductRequest, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+                .ForMember(dest => dest.Created, opt => opt.Ignore())
+                .ForMember(dest => dest.Updated, opt => opt.Ignore());
+
+            CreateMap<UpdateProductRequest, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+                .ForMember(dest => dest.Created, opt => opt.Ignore())
+                .ForMember(dest => dest.Updated, opt => opt.Ignore());
+        }
+    }
+}
