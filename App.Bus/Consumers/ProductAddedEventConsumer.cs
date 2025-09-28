962351c22a7fdd03fa1cf30ceff145c3f1298c93
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-28 14:16:07 +0300

    feat(consumer) : created

---
+++ App.Bus/Consumers/ProductAddedEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using App.Domain.Events.ProductEvents;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace App.Bus.Consumers
+{
+    public class ProductAddedEventConsumer : IConsumer<ProductAddedEvent>
+    {
+        private readonly ILogger<ProductAddedEventConsumer> _logger;
+
+        public ProductAddedEventConsumer(ILogger<ProductAddedEventConsumer> logger) => _logger = logger;
+        public Task Consume(ConsumeContext<ProductAddedEvent> context)
+        {
+            var msg = context.Message;
+            _logger.LogInformation($"Received Event: {msg.Id} - {msg.Name} - {msg.Price}");
+        return Task.CompletedTask;      
+         }
+    }
+}
