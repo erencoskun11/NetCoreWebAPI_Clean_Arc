0dad8293bfb9a460e8220bc1dc1810c06ce4c7e1
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-28 14:15:07 +0300

    feat(consumer) : created

---
+++ App.Bus/Consumers/CategoryDeletedEventConsumer.cs
@@ -0,0 +1,23 @@
+﻿using App.Domain.Events.CategoryEvents;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Bus.Consumers
+{
+    public class CategoryDeletedEventConsumer : IConsumer<CategoryDeletedEvent>
+    {
+        private readonly ILogger<CategoryDeletedEventConsumer> _logger;
+        public CategoryDeletedEventConsumer (ILogger<CategoryDeletedEventConsumer> logger) =>_logger = logger;
+        public Task Consume(ConsumeContext<CategoryDeletedEvent> context)
+        {
+            var msg = context.Message;
+            _logger.LogInformation($"received event : {msg.Id} - {msg.Name} ");
+            return Task.CompletedTask;
+        }
+    }
+}
