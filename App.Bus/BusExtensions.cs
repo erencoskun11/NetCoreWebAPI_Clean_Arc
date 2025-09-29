c59d6faac481cea2d1b72bdeb9ddced8241b3570
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-29 23:27:41 +0300

    added ProductionDeletedEventConsumer CategoryDeletedEventConsumer, CategoryDeletedEventConsumer,

--- App.Bus/BusExtensions.cs
+++ App.Bus/BusExtensions.cs
@@ -21,24 +21,53 @@ namespace App.Bus
             services.AddMassTransit(x =>
             {
                 x.AddConsumer<ProductAddedEventConsumer>();
+                x.AddConsumer<ProductDeletedEventConsumer>();
                 x.AddConsumer<CategoryAddedEventConsumer>();
+                x.AddConsumer<CategoryDeletedEventConsumer>();
+
+                x.AddConsumer<ReserveProductConsumer>();
+                x.AddConsumer<ReserveCategoryConsumer>();
                 x.UsingRabbitMq((context, cfg) =>
                 {
                     cfg.Host(new Uri(serviceBusOptions!.Url), h =>
                     {
-                       
+
                     });
-                    //for product
+                    
                     cfg.ReceiveEndpoint(ServiceBusConst.ProductAddedEventQueueName,
                         e =>
                         {
                             e.ConfigureConsumer<ProductAddedEventConsumer>(context);
+                        });
+                    cfg.ReceiveEndpoint(ServiceBusConst.ProductDeletedEventQueueName,
+                        e =>
+                        {
+                            e.ConfigureConsumer<ProductDeletedEventConsumer>(context);
                         });
                     //for category
                     cfg.ReceiveEndpoint(ServiceBusConst.CategoryAddedEventQueueName, e =>
                     {
                         e.ConfigureConsumer<CategoryAddedEventConsumer>(context);
                     });
+                    cfg.ReceiveEndpoint(ServiceBusConst.CategoryDeletedEventQueueName,
+                        e =>
+                        {
+                            e.ConfigureConsumer<CategoryDeletedEventConsumer>(context);
+                        });
+
+                    cfg.ReceiveEndpoint(ServiceBusConst.ReserveProductCommandQueueName, e =>
+                    {
+                        e.ConfigureConsumer<ReserveProductConsumer>(context);
+
+                        e.UseMessageRetry(r => r.Interval(3, TimeSpan.FromSeconds(5)));
+                    });
+                    cfg.ReceiveEndpoint(ServiceBusConst.ReserveCategoryCommandQueueName, e =>
+                    {
+                    e.ConfigureConsumer<ReserveCategoryConsumer>(context);
+
+                    e.UseMessageRetry(r =>r.Interval(3, TimeSpan.FromSeconds(5)));
+                    });
+
                 });
             });
         }
