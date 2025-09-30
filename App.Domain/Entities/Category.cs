0f57c5df64a7f75885871015f93c93c23e0f7274
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-30 23:01:27 +0300

    feat(entity) : created

---
+++ App.Domain/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using App.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Entities
+{
+    public class Category :BaseEntity<int>, IAuditEntity
+    {
+        public string Name { get; set; } = default!;
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
