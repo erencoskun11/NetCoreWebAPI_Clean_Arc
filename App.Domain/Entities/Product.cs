c4500a244a48952fac31858721806b5747e1588f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-30 23:02:08 +0300

    created

---
+++ App.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using App.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Entities
+{
+    public class Product :BaseEntity<int>, IAuditEntity
+    {
+        public string Name { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
