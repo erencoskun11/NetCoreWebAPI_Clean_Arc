34e2fe656f8073d8b193812900a0c1b49e535280
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-05 19:09:43 +0300

    feat(extensions) created

---
+++ App.Persistance/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using App.Application.Contracts.Persistence;
+using App.Persistance.Categories;
+using App.Persistance.Products;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Persistance.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            // open-generic registration: IGenericRepository<TEntity, TId> => GenericRepository<TEntity, TId>
+            services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>));
+
+            // concrete repositories
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
