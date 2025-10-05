ef0455721f8beae196ced4718d12b1da0d0133e3
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-05 19:10:29 +0300

    created

---
+++ App.Persistance/Interceptors/AuditDbContextInterceptor.cs
@@ -0,0 +1,46 @@
+﻿using App.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace App.Persistance.Interceptors
+{
+    public class AuditDbContextInterceptor : SaveChangesInterceptor
+    {
+        private static readonly Dictionary<EntityState, Action<DbContext, IAuditEntity>> _behaviors = new()
+        {
+            { EntityState.Added, AddBehavior },
+            { EntityState.Modified, ModifiedBehavior }
+        };
+
+        private static void AddBehavior(DbContext context, IAuditEntity auditEntity)
+        {
+            auditEntity.Created = DateTime.UtcNow;
+            context.Entry(auditEntity).Property(x => x.Updated).IsModified = false;
+        }
+
+        private static void ModifiedBehavior(DbContext context, IAuditEntity auditEntity)
+        {
+            context.Entry(auditEntity).Property(x => x.Created).IsModified = false;
+            auditEntity.Updated = DateTime.UtcNow;
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entityEntry in eventData.Context!.ChangeTracker.Entries().ToList())
+            {
+                if (entityEntry.Entity is not IAuditEntity auditEntity) continue;
+
+                if (entityEntry.State == EntityState.Added || entityEntry.State == EntityState.Modified)
+                {
+                    _behaviors[entityEntry.State](eventData.Context, auditEntity);
+                }
+            }
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
+
