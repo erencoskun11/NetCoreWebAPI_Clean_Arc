df410766040046284ef4559a667d748f8daa595a
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-05 19:09:19 +0300

    feat(repository) : created

---
+++ App.Persistance/Categories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using App.Application.Contracts.Persistence;
+using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Persistance.Categories
+{
+    public class CategoryRepository : GenericRepository<Category,int>, ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Category> GetCategoryWithProducts()
+        {
+            return _context.Categories.Include(x=>x.Products).AsNoTracking();
+        }
+
+        public Task<Category?> GetCategoryWithProductsAsync(int id)
+        {
+            return _context.Categories.Include(x => x.Products).FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
