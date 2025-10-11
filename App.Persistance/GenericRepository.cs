0b3309e32beb15a077cbb5f56092f284c924e2af
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-11 19:34:55 +0300

    feat(repository) : created

---
+++ App.Persistance/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using App.Application.Contracts.Persistence;
+using App.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace App.Persistance
+{
+    public class GenericRepository<T, TId> : IGenericRepository<T, TId>
+        where T : BaseEntity<TId>
+        where TId : struct
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        // IQueryable means it does not fetch the data immediately
+        public Task<bool> AnyAsync(TId id) => _dbSet.AnyAsync(x => x.Id!.Equals(id));
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate) => _dbSet.AnyAsync(predicate);
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return _dbSet.ToListAsync();
+        }
+
+        public Task<List<T>> GetAllPagedAsync(int pageNumber, int pageSize)
+        {
+            return _dbSet.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+
+        public async Task<T?> GetByIdAsync(TId id)
+        {
+            var vt = _dbSet.FindAsync(id);
+            return await vt;
+        }
+
+        public async ValueTask AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+}
